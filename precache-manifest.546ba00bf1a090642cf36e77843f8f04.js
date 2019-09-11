a1db9c3cca653e19b617be2e04f88031cb512355
Prem <premsahni0501@gmail.com>
2019-09-11 18:32:45 +0530

    Updates

---
+++ precache-manifest.546ba00bf1a090642cf36e77843f8f04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78522cbc8029b0f75dcdd59215781030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e6f762a64084dfd7ee7",
+    "url": "/static/css/main.1bb785d4.chunk.css"
+  },
+  {
+    "revision": "ff092108168f8d57eeee",
+    "url": "/static/js/2.c81c64a0.chunk.js"
+  },
+  {
+    "revision": "2e6f762a64084dfd7ee7",
+    "url": "/static/js/main.ed159efd.chunk.js"
+  },
+  {
+    "revision": "9c62f656079458ad5dda",
+    "url": "/static/js/runtime~main.6479e899.js"
+  },
+  {
+    "revision": "64ac9ec339d7725ef47ce25abbbd720f",
+    "url": "/static/media/user.64ac9ec3.png"
+  }
+]);
